bea9643c7e177615df150b5aff6329f6f936ef9f
Efe <efeylmazzz1@gmail.com>
2024-11-26 22:55:43 +0300

    Delete NesneProg directory

--- NesneProg/Products/main.py
+++
@@ -1,8 +0,0 @@
-from Helpers.product_helper import ProductHelper
-
-products = ProductHelper.create_item_from_text('products.txt')
-
-ProductHelper.display_product_info(products)
-
-total_balance = ProductHelper.get_total_balance(products)
-print(f"Total Price (KDV with the inclusion): ${total_balance:.2f}")
